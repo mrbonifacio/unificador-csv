a1a7dbbdceeb2907636eb36a62dd2051465479f9
Vinícius Bonifácio Pereira <mrbonifaciobr@gmail.com>
2024-01-30 11:12:50 -0300

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+import csv
+import os
+
+pasta_csv = r'baseCarteira'
+csv_unificado = []
+
+for arquivo in os.listdir(pasta_csv):
+    if arquivo.endswith('.csv'):
+        caminho_arquivo = os.path.join(pasta_csv, arquivo)
+        with open(caminho_arquivo, mode='r', encoding='utf-16') as arquivo_csv:
+            ler_csv = csv.reader(arquivo_csv, delimiter='\t')  # Define o delimitador como tabulação
+            csv_unificado.extend(list(ler_csv)[6:])  # Pula as primeiras 6 linhas
+
+with open(r'V:\\Centro de Custos\\Cooperativa de Crédito\\PA00 - ADMINISTRATIVO\\DESENVOLVIMENTO DE SISTEMAS\\Relatoriounificado\\Base_Minha_Carteira.csv', mode='w', newline='', encoding='utf-16') as csv_final:
+    base = csv.writer(csv_final, delimiter='\t')  # Define o delimitador como tabulação
+    for linha in csv_unificado:
+        base.writerow(linha)
